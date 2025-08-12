041542c64ff3e36b8606c1c2a9fdb997a318a35a
takuya <cdwbh564@yahoo.co.jp>
2025-08-13 01:05:01 +0900

    feat: 管理者認証ミドルウェアを追加し、認証方法を更新

--- app/Http/Controllers/Admin/HolidayController.php
+++ app/Http/Controllers/Admin/HolidayController.php
@@ -10,7 +10,7 @@ class HolidayController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('auth.admin');
     }
 
     public function index()
